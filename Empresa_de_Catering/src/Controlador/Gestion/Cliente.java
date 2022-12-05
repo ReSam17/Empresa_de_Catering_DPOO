1e69f86ffd049305902f5a07b5cc62f897f56ed1
ReSam17 <reysonhernandezvilla@gmail.com>
2022-12-05 13:58:01 -0500

    Parte de lisndra

--- Empresa_de_Catering/src/Controlador/Gestion/Cliente.java
+++ Empresa_de_Catering/src/Controlador/Gestion/Cliente.java
@@ -7,5 +7,67 @@ package Controlador.Gestion;
 
 
 public class Cliente {
-    
+    private String Nombre;
+    private Pedido Pedido;
+    private Pedido[]PedidoRealizados;
+    private String Email;
+    private Boolean Nuevo;
+    private String Estado;
+    public Cliente(){
+    this.Nombre= " ";
+    this.Pedido= Pedido;
+    this. PedidosRealizados= Pedido[];
+    this.Email=" " ;
+    this.Nuevo= " ";
+    this.Estado=" " ;
+    }
+    public Cliente(String Nombre, Pedido Pedido,Pedido[]PedidosRealizados,String Email, Boolean Nuevo,String Estado){
+    this.Nombre=Nombre;
+    this.Pedido=Pedido;
+    this.PedidosRealizados=PedidosRealizados;
+    this.Email=Email;
+    this.Nuevo=Nuevo;
+    this.Estado=Estado;
+    }
+    public String get Nombre(){
+    return this.Nombre;
+    }
+    public void set Nombre (String Nuevo Nombre){
+    this.Nombre=Nombre;
+    }
+    public Pedido get Pedido(){
+    return this.Pedido;
+    }
+    public void set Pedido(Pedido Nuevo Pedido){
+    this.Pedido=Pedido;
+    }
+    public Pedido[] get PedidosRealizados(){
+    return this.PedidosRealizados;}
+    public void PedidosRealizados(Pedido[] Nuevo PedidosRealizados){
+    this.PedidosRealizados=PedidosRealizados;
+    }
+    public String get Email (){
+    return this.Email;
+    }
+    public void set Email(String Nuevo Email){
+    this.Email=Email;
+    }
+    public Boolean get Nuevo (){
+    return this.Nuevo;
+    }
+    public void set Nuevo(Boolean Nuevo Nuevo){
+    this.Nuevo=Nuevo;
+    }
+    public String get Estado (){
+    return this. Estado;
+    }
+    public void set Estado(String Nuevo Estado){
+    this.Estado;
+    }
+    public RealizarPedido(Pedido){
+    return this.Pedido;
+    }
+    public Boolean AceptacionPropuesta(Propuesta Propuesta){
+    return this.Propuesta;
+    }
 }
--- Empresa_de_Catering/src/Controlador/Gestion/Pedido.java
+++ Empresa_de_Catering/src/Controlador/Gestion/Pedido.java
@@ -7,5 +7,73 @@ package Controlador.Gestion;
 
 
 public class Pedido {
-    
+    private String Fecha;
+    private String TipoDeEvento;
+    private int CantidadDePersonas;
+    private Propuesta Propuesta;
+    private FEmitida Factura;
+    private Boolean Veganos;
+    private String[] Alergias;
+    public Pedido(){
+    this. Fecha= " ";
+    this. TipoDeEvento=" ";
+    this.CantidadDePersonas=0;
+    this. Propuesta="";
+    this.Factura= 0;
+    this. Veganos = " ";
+    this.Alergias= [];
+    }
+    public Pedido(String Fecha,String TipoDeEvento, int CantidadDePersonas, Propuesta Propuesta,FEmitida Factura, Boolean Veganos,String[]Alergias){
+    this.Fecha=Fecha;
+    this.TipoDeEvento=TipoDeEvento;
+    this.CantidadDePersonas=CantidadDePersonas;
+    this.Propuesta=Propuesta;
+    this.Factura=Factura;
+    this.Veganos=Veganos;
+    this.Alergias=Alergias;
+    }
+    public String get Fecha(){
+    return this. Fecha;
+    }
+    public void set Fecha(String Nueva Fecha){
+    this.Fecha=Nueva Fecha;
+    }
+    public String TipoDeEvento(){
+    return this. TipoDeEvento;
+    }
+    public void set TipoDeEvento(String Nuevo TipoDeEvento){
+    this.TipoDeEvento=Nuevo TipoDeEvento;
+    }
+    public int get CantidadDePersonas(){
+    return this.CantidadDePersonas;
+    }
+    public void set CantidadDePersonas(int Nueva CantidadDePersonas){
+    this.CantidadDePersonas=Nueva CantidadDePersonas;
+    }
+    public Propuesta get Propuesta(){
+    return this.Propuesta;
+    }
+    public void set Propuesta (Propuesta nuevaPropuesta){
+    this.Propuesta=nuevaPropuesta;
+    }
+    public FEmetida get Factura(){
+    return this.Factura;
+    }
+    public void set Factura(FEmitida nuevaFactura){
+    this.Factura=nuevaFactura;
+    }
+    public Boolean get Veganos (){
+    return this.Veganos;
+    }
+    public void set Veganos(Boolean nuevoVeganos){
+    this.Veganos=nuevoVeganos;
+    }
+    public String get Alergias(){
+    return this.Alergias;
+    }
+    public void set Alergias(String NuevaAlergias){
+    this.Alergias=NuevaAlergias;
+    }
+
+
 }
